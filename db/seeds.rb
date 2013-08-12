0950bf6d6563523e96ac037ff6347c57f2b3a60f
wbvg-mac <will@mylittleshowpony.com>
2013-08-12 16:21:15 +1000

    trying to dump db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
-User.destroy_all
-Profile.destroy_all
-Status.destroy_all
+# User.destroy_all
+# Profile.destroy_all
+# Status.destroy_all
 
 u1 = User.create(:email => 'willy@gmail.com', :password => '123456', :password_confirmation => '123456', :occupation => "UX | UI Designer",   :location => "Sydney", :avatar_file_name => "willy_fb_profile.jpg", :avatar_content_type => "image/jpeg", :avatar_file_size => "38103", :username => "willy")
 
 u2 = User.create(:email => 'missy@gmail.com', :password => '123456', :password_confirmation => '123456', :occupation => "UX | UI Designer",   :location => "Sydney", :avatar_file_name => "willy_fb_profile.jpg", :avatar_content_type => "image/jpeg", :avatar_file_size => "38103", :username => "willy")
-
-u3 = User.create(:email => 'larry@gmail.com', :password => '123456', :password_confirmation => '123456', :occupation => "UX | UI Designer",   :location => "Sydney", :avatar_file_name => "willy_fb_profile.jpg", :avatar_content_type => "image/jpeg", :avatar_file_size => "38103", :username => "larry")
 
 
 s1 = Status.create(:category => "UX | UI Designer", :status => "Hi I need help", :need_help => true)
@@ -16,9 +14,8 @@ s4 = Status.create(:category => "UX | UI Designer", :status => "Hi I need help w
 s5 = Status.create(:category => "Back-End Developer", :status => "Hi I need help", :need_help => false)
 s6 = Status.create(:category => "Jack of All Trades", :status => "Hi I am looking to partner in Sydney area", :need_help => true)
 
-u1.statuses << [s1,s2]
-u2.statuses << [s3,s4]
-u3.statuses << [s5,s6]
+u1.statuses << [s1,s2,s5]
+u2.statuses << [s3,s4,s6]
 
 p1 = Profile.create(:label => 'Javascript', :value => 100, :chapter => 'Junior Dev')
 p2 = Profile.create(:label => 'Web Design', :value => 100, :chapter => 'Junior Dev')
@@ -29,9 +26,9 @@ p6 = Profile.create(:label => 'Axure Wireframing', :value => 100, :chapter => 'S
 p7 = Profile.create(:label => 'Prototyping', :value => 70, :chapter => 'Sydney Web Designer')
 
 u1.profiles << [p5,p6,p7]
-u2.profiles << [p3,p4]
-u3.profiles << [p1,p2]
+u2.profiles << [p1,p2,p3,p4]
 
 u1.save
 u2.save
-u3.save
+
+
